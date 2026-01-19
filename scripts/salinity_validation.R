95e0dc77f2217e539c1e4fa3530a67d0aa4e1e2b
isaakh10 <isaak.haberman@hakai.org>
2026-01-19 14:20:09 -0800

    testing CSV writing

--- scripts/salinity_validation.R
+++ scripts/salinity_validation.R
@@ -1,2 +1,8 @@
 IndianArm <- read.csv("staroddi-data/2025/S13688_IndianArm_REFORMATTED.csv", fileEncoding = "latin1", check.names = FALSE)
-View(data)
+IndianArm$site <- "Indian Arm"
+
+PenderHarbour <- read.csv("staroddi-data/2025/S13689_PenderHarbour_REFORMATTED.csv", fileEncoding = "latin1", check.names = FALSE)
+PenderHarbour$site <- "Pender Harbour"
+
+df <- rbind(IndianArm, PenderHarbour)
+write.csv(df, "staroddi-data/2025/test.csv")
